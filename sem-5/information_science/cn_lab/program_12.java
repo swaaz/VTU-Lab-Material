5007dc800e3183742075f0421aa9890f3148a1c6
swaaz <swaasthik.shetty07@gmail.com>
2021-01-19 00:07:04 +0530

    added leaky  program

---
+++ sem-5/information_science/cn_lab/program_12.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+public class leaky {
+    public static void main(String args[]){
+        int inp[]=new int[30];
+        int count=0;
+        int drop=0;
+        int mini,i;
+        System.out.println("Enter Size Of the bucket");
+        Scanner sc=new Scanner(System.in);
+        int cap=sc.nextInt();
+        System.out.println("Enter the output rate");
+        int out=sc.nextInt();
+        System.out.println("Enter the number of packets");
+        int n=sc.nextInt();
+        System.out.println("Enter size of the packets");
+        for(i=0;i<n;i++)
+            inp[i]=sc.nextInt();
+        System.out.println("Second\tPacket Received\tPacket Sent\tPacket Left\tDrop");
+        for(i=0;i<n;i++){
+            count+=inp[i];
+            if(count>cap){
+                drop=count-cap;
+                count=cap;
+            }
+            if(count>out){
+                mini=out;
+                count-=out;
+            }
+            else{
+                mini=count;
+                count=0;
+            }
+            System.out.println(i+1+"\t"+inp[i]+"\t\t"+mini+"\t\t"+count+"\t\t"+drop);
+        }
+        for(;count!=0;i++){                    //if data is remaining in bucket after input becomes null
+            if(count>cap){
+                drop=count-cap;
+                count=cap;
+            }
+            if(count>out){
+                mini=out;
+                count-=out;
+            }
+            else{
+                mini=count;
+                count=0;
+            }
+            System.out.println(i+1+"\t"+inp[i]+"\t\t"+mini+"\t\t"+count+"\t\t"+drop);
+        }
+        sc.close();
+    }
+}
+
+
+import java.util.Scanner;
+public class leaky {
+    public static void main(String args[]){
+        int inp[]=new int[30];
+        int count=0;
+        int drop=0;
+        int mini,i;
+        System.out.println("Enter Size Of the bucket");
+        Scanner sc=new Scanner(System.in);
+        int cap=sc.nextInt();
+        System.out.println("Enter the output rate");
+        int out=sc.nextInt();
+        System.out.println("Enter the number of packets");
+        int n=sc.nextInt();
+        System.out.println("Enter size of the packets");
+        for(i=0;i<n;i++)
+            inp[i]=sc.nextInt();
+        System.out.println("Second\tPacket Received\tPacket Sent\tPacket Left\tDrop");
+        for(i=0;i<n;i++){
+            count+=inp[i];
+            if(count>cap){
+                drop=count-cap;
+                count=cap;
+            }
+            if(count>out){
+                mini=out;
+                count-=out;
+            }
+            else{
+                mini=count;
+                count=0;
+            }
+            System.out.println(i+1+"\t"+inp[i]+"\t\t"+mini+"\t\t"+count+"\t\t"+drop);
+        }
+        for(;count!=0;i++){                    //if data is remaining in bucket after input becomes null
+            if(count>cap){
+                drop=count-cap;
+                count=cap;
+            }
+            if(count>out){
+                mini=out;
+                count-=out;
+            }
+            else{
+                mini=count;
+                count=0;
+            }
+            System.out.println(i+1+"\t"+inp[i]+"\t\t"+mini+"\t\t"+count+"\t\t"+drop);
+        }
+        sc.close();
+    }
+}
+
+
+
